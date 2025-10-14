f3dc17b10c28a87c19906fc84d26d527c8edfb17
artattack_fuso <artattack_fuso@tiscali.it>
2025-10-14 19:54:44 +0200

    removed unuseful csv

--- app/Home.py
+++ app/Home.py
@@ -2,19 +2,21 @@ import pandas as pd
 import streamlit as st
 from datetime import datetime, timedelta
 
-
-# Upload del file
-uploaded_file = "app/resources/AllCalendarsMerged.xlsx"
-
+# Configura la pagina
 st.set_page_config(page_title="Home", layout="wide")
 st.write(
     "<style>div.block-container{padding-top:0.5rem;}</style>", unsafe_allow_html=True
 )
 st.markdown("Partite Settore di Base")
 
+uploaded_file = "app/resources/AllCalendarsMerged.xlsx"
+@st.cache_data
+def load_excel(file_path):
+    return pd.read_excel(file_path, engine="openpyxl")
+
 try:
     # Legge il file Excel
-    df = pd.read_excel(uploaded_file, engine="openpyxl")
+    df = load_excel(uploaded_file)
     
     # ✅ Parsing della colonna "Data" (assicurati che il nome corrisponda esattamente)
     df["Data"] = pd.to_datetime(df["Data"], format="%d/%m/%Y", errors='coerce')
@@ -25,7 +27,6 @@ try:
     df = df[(df["Data"] >= oggi) & (df["Data"] <= settimana_prossima)]
     df = df.sort_values(by=["Data", "Casa"], ascending=[True, True])
 
-    # --- FILTRO DATA ---
      # Creiamo due colonne affiancate per i filtri
     col1, col2, col3 = st.columns([2, 2, 6])  # 1:1:2 dimension ratio
 
@@ -33,30 +34,11 @@ try:
         # Filtro testo generico su Casa e Ospite
         testo_filtrato = st.text_input(label="",placeholder="Cerca per Squadra", icon="⚽")
 
-    # with col2:
-    #     # Filtro data
-    #     min_date = df["Data"].min().date()
-    #     max_date = df["Data"].max().date()
-    #     selected_date_range = st.date_input(
-    #         "Filtra per intervallo Data",
-    #         value=(min_date, max_date),
-    #         min_value=min_date,
-    #         max_value=max_date,
-    #     )
-
     # Applichiamo i filtri
     if testo_filtrato:
         mask_casa = df["Casa"].astype(str).str.contains(testo_filtrato, case=False, na=False)
         mask_ospite = df["Ospite"].astype(str).str.contains(testo_filtrato, case=False, na=False)
         df = df[mask_casa | mask_ospite]
-
-    # if isinstance(selected_date_range, tuple) and len(selected_date_range) == 2:
-    #     df = df[
-    #         (df["Data"].dt.date >= selected_date_range[0]) &
-    #         (df["Data"].dt.date <= selected_date_range[1])
-    #     ]
-    # else:
-    #     df = df[df["Data"].dt.date == selected_date_range]
 
     # Nascondi colonna Indirizzo solo nella visualizzazione
     columns_to_hide = ["Indirizzo", "A/R", "Girone", "Giornata", "Federazione"]
