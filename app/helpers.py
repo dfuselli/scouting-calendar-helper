2d8255ead401b0a1723011e780a4864eb96529ed
artattack_fuso <artattack_fuso@tiscali.it>
2026-02-28 10:49:41 +0100

    fix: compress category method

--- app/helpers.py
+++ app/helpers.py
@@ -1,16 +1,16 @@
 shortcut_categorie = {
-    "Giovanissimi": "Giov.",
-    "Esordienti": "Esord.",
-    "Pulcini": "Pulc.",
-    "MiniPulcini": "MiniPulc.",
+    "GIOVANISSIMI": "Giov.",
+    "ESORDIENTI": "Esord.",
+    "PULCINI": "Pulc.",
+    "MINIPULCINI": "MiniPulc.",
 }
 
 def merge_categoria_federazione(categoria):
     if categoria:
         compressed = ""
         for word in categoria.split(" "):
-            if word in shortcut_categorie:
-                compressed += shortcut_categorie[word]
+            if word.upper() in shortcut_categorie:
+                compressed += shortcut_categorie[word.upper()] + " "
             else:
                 compressed += word
             compressed += " "  # aggiunge spazio tra le parole
