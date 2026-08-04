6266d899b9fc2c51278e259c78bea70572ca3071
artattack_fuso <artattack_fuso@tiscali.it>
2026-08-04 22:23:29 +0200

    move link gdrrive as secret

--- app/common/data_handler.py
+++ app/common/data_handler.py
@@ -1,3 +1,4 @@
+import os
 import sqlite3
 import tempfile
 from datetime import UTC, datetime, time, timedelta
@@ -7,9 +8,8 @@ import pandas as pd
 import streamlit as st
 from helpers import merge_categoria_federazione
 
-calendar_file_path = "app/resources/AllCalendarsMerged.xlsx"
 # db_path = "app/resources/scouting_assistant.db" #
-gdrive_db_path = "https://drive.google.com/uc?id=1mDEHXgx53oxzAEOXETRXGwPXeYXlmiWx"
+gdrive_db_path = st.secrets.get("gdrive_db_path", os.environ.get("GDRIVE_DB_PATH"))
 
 
 @st.cache_data(ttl=600)
@@ -54,51 +54,6 @@ def init_db():
 
 
 init_db()
-
-
-@st.cache_data(ttl="15m")
-def load_calendar_data(filter_next_7_days=True):
-    df = pd.read_excel(calendar_file_path, engine="openpyxl")
-    df = df[df["Data"].notna()]
-    df["Data"] = pd.to_datetime(df["Data"], format="%d/%m/%Y", errors="coerce")
-
-    # Filtra per i prossimi 7 giorni
-    if filter_next_7_days:
-        oggi_mezzanotte = datetime.combine(datetime.now(tz=UTC), time.min)
-        stasera_mezzanotte = datetime.combine(datetime.now(tz=UTC), time.max)
-        settimana_prossima = stasera_mezzanotte + timedelta(days=8)
-        df = df[(df["Data"] >= oggi_mezzanotte) & (df["Data"] < settimana_prossima)]
-
-    # Aggiungi colonna ID univoco per tracciabilità
-    if df.empty:
-        st.warning(
-            "Nessuna partita trovata nei prossimi 7 giorni."
-            if filter_next_7_days
-            else "Il calendario è vuoto."
-        )
-        return df
-    else:
-        df["ID"] = df.apply(
-            lambda row: (
-                f"{row['Data'].strftime('%Y%m%d')}_{row['Categoria']}_{row['Casa']}_{row['Ospite']}"
-            ),
-            axis=1,
-        )
-        df["_TimeSort"] = pd.to_datetime(
-            df["Data"].dt.strftime("%Y-%m-%d") + " " + df["Ora"]
-        )
-        df["Time"] = df["_TimeSort"].dt.strftime("%d/%m %H:%M")
-        df["Fascia"] = df.apply(
-            lambda row: (
-                f"{'🟡' if 'CSI' in row['Federazione'].upper() else '🔵'}{merge_categoria_federazione(row['Categoria'])}"
-            ),
-            axis=1,
-        )
-        df["Selezionato"] = False
-        df = df.sort_values(by=["_TimeSort", "Casa"], ascending=[True, True]).drop(
-            columns="_TimeSort"
-        )
-        return df
 
 
 st.cache_data(ttl="15m")
