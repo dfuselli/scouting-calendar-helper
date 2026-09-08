5f15a2131b868f601375e37fbac233f117855a2f
artattack_fuso <artattack_fuso@tiscali.it>
2026-09-08 21:43:15 +0200

    fix: mobile width

--- app/pages/1_⚽_CalendarHelper.py
+++ app/pages/1_⚽_CalendarHelper.py
@@ -105,11 +105,11 @@ def _render_table(df_visible) -> None:
         hide_index=True,
         on_change=_handle_change,
         column_config={
-            "Selezionato": st.column_config.CheckboxColumn("", width=35, pinned=True),
-            "Time": st.column_config.TextColumn("Data", width=10, disabled=True),
+            "Selezionato": st.column_config.CheckboxColumn("", width=25, pinned=True),
+            "Time": st.column_config.TextColumn("Data", width=30, disabled=True),
             "Casa": st.column_config.TextColumn("Casa", width=50, disabled=True),
             "Ospite": st.column_config.TextColumn("Ospite", width=50, disabled=True),
-            "Fascia": st.column_config.TextColumn("Fascia", width=15, disabled=True),
+            "Fascia": st.column_config.TextColumn("Fascia", width=10, disabled=True),
         },
     )
 
